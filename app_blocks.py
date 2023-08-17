40ef1009ddc3343ec618db8db84488f685783811
hansel <hanselpetter0927@gmail.com>
2023-08-17 16:51:01 +0530

    Add application file

--- app_blocks.py
+++ app_blocks.py
@@ -13,7 +13,7 @@ title = "Tesseract OCR"
 description = "Gradio demo for Tesseract. Tesseract is an open source text recognition (OCR) Engine."
 article = "<p style='text-align: center'><a href='https://tesseract-ocr.github.io/' target='_blank'>Tesseract documentation</a> | <a href='https://github.com/tesseract-ocr/tesseract' target='_blank'>Github Repo</a></p>"
 examples = [
-    ["examples/weird_unicode_math_symbols.png", []]
+    ["examples/weird_unicode_math_symbols.png", []],
     ["examples/eurotext.png", ["eng"]],
     ["examples/tesseract_sample.png", ["jpn", "eng"]],
     ["examples/chi.jpg", ["HanS", "HanT"]],
